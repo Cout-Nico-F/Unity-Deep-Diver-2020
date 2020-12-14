53fe930e6432b320ee9a27bf655dd0f5a9d806ac
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-12-14 01:15:05 -0300

    Falling items respawn and seacucumber is on the scene!

---
+++ Starting proyect/Assets/FallingItemRespawnScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingItemRespawnScript : MonoBehaviour
+{
+    Vector3 initialPos;   
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckRespawn();
+    }
+
+    //maybe can add some randomize to the position
+    private void CheckRespawn()
+    {
+        if (transform.position.y <= -3.4)
+        {
+            transform.position = initialPos + Vector3.up * 3;
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0.1f,-0.1f);
+        }
+    }
+}
