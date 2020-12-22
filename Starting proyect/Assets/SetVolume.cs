79fc4ea452e1bf8c8736c8d2aa05d68b0c4f596d
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-12-22 19:00:16 -0300

    Volume is now constant between scenes

--- Starting proyect/Assets/SetVolume.cs
+++ Starting proyect/Assets/SetVolume.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
+    private Slider slider;
 
+    private void Awake()
+    {
+        slider = this.gameObject.GetComponent<Slider>();
+    }
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("Volume", 0.5f);
+    }
     public void Volume(float sliderValue)
     {
         mixer.SetFloat("Volume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("Volume", sliderValue);
     }
 }
