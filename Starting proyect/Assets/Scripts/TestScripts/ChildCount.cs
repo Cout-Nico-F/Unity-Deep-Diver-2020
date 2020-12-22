fb4a25101d5c9473b8dd38bda44d51c44decc7ac
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-12-22 17:24:10 -0300

    ChildCount utility script. we have 129 collectable today.

---
+++ Starting proyect/Assets/Scripts/TestScripts/ChildCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildCount : MonoBehaviour
+{
+    // This script is useful to count how many childs has a gameobject. 
+
+    GameObject obj;
+    void Awake()
+    {
+        obj = this.gameObject;
+        Debug.Log(obj.name + " has " + obj.transform.childCount + " children");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(obj.name + " has " + obj.transform.childCount + " children");
+    }
+}
