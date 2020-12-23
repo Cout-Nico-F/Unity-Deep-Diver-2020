85f487af8d8bdbdc3d753627d01fc0abf053e79b
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-12-23 20:11:30 -0300

    Bug found and cleared on the SFX and volume Handling.

---
+++ Starting proyect/Assets/Scripts/Audio/SetMusicVolume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetMusicVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    private Slider musicSlider;
+
+    private void Awake()
+    {
+        musicSlider = this.gameObject.GetComponent<Slider>();
+    }
+    private void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("Music_Volume_Value", 0.5f);
+    }
+    public void Music_Volume(float sliderValue)
+    {
+        mixer.SetFloat("Music_Volume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("Music_Volume_Value", sliderValue);
+    }
+
+}
--- Starting proyect/Assets/Scripts/Audio/SetSFXVolume.cs
+++ Starting proyect/Assets/Scripts/Audio/SetSFXVolume.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class SetVolume : MonoBehaviour
+public class SetSFXVolume : MonoBehaviour
 {
     public AudioMixer mixer;
-    private Slider musicSlider;
+    private Slider sfxSlider;
 
     private void Awake()
     {
-        musicSlider = this.gameObject.GetComponent<Slider>();
+        sfxSlider = this.gameObject.GetComponent<Slider>();
     }
     private void Start()
     {
-        musicSlider.value = PlayerPrefs.GetFloat("Music_Volume_Value", 0.5f);
-    }
-    public void Music_Volume(float sliderValue)
-    {
-        mixer.SetFloat("Music_Volume", Mathf.Log10(sliderValue) * 20);
-        PlayerPrefs.SetFloat("Music_Volume_Value", sliderValue);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFX_Volume_Value", 0.5f);
     }
     public void SFX_Volume(float sliderValue)
     {
