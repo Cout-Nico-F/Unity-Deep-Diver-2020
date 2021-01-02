c71f5fb34871d6e8e8366d75c449526b47635edb
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-01-02 18:44:22 -0300

    new sliders on main menu

---
+++ Starting proyect/Assets/SliderHandle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderHandle : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        transform.localScale = transform.localScale + Vector3.one;
+        Debug.Log("Probando OnmouseOVer");
+    }
+}
